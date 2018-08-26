411650eac2ae16b2e1fd9deef034d96f452e42a3
sheez <blaat2k@mail.com>
2018-08-26 14:27:59 +0200

    Add initial setup PostQuiz; show amount of correct answers and message

--- src/components/PostQuiz.js
+++ src/components/PostQuiz.js
@@ -7,15 +7,39 @@ class PostQuiz extends Component {
 
   goToQuestions = () => {
     const { history } = this.props
-    
+   
     history.push("/")
   }
 
   render() {
-    console.log("state: " + this.props.location.state.correctAnswers)
+    const { correctAnswers, amountOfQuestions } = this.props.location.state
+    let performanceMessage
+
+    switch(correctAnswers) {
+      case 0 :
+      case 1 : {
+        performanceMessage = "poor performance"
+        break;
+      }
+      case 2: {
+        performanceMessage = "well done!"
+        break;
+      }
+      case 3: {
+        performanceMessage = "superB!"
+        break;
+      }
+      default: {
+        performanceMessage = "error, bad programming"
+      }
+    }
+
     return (
       <Fragment>
         <h1>Quiz ended</h1>
+        <p>You answered {correctAnswers} question{correctAnswers === 1 ? '' : '(s)'} out of {amountOfQuestions} correctly</p>
+        <p>{performanceMessage}</p>
+
         <button onClick={this.goToQuestions}>reset</button>
       </Fragment>
     );
