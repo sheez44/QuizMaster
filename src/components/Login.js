706c1ee4ef846d21eacb9e1667be13a1751ac086
sheez <blaat2k@mail.com>
2018-08-26 10:51:33 +0200

    Improve Login; fetch password + username on submit (no real validation)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Login = () => (
-  <form>
-    <fieldset>
-      <label for="userName">Username: </label>
-      <input type="text" id="userName" />
-      <label for="password">Password: </label>
-      <input type="password" id="password" />
-    </fieldset>
-  </form>
-);
+class Login extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      validated: false
+    }
+  }
+  
+  myUsernameInput = React.createRef();
+  myPasswordInput = React.createRef()
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const { validated } = this.state
+    const { history } = this.props
+
+    const userName = this.myUsernameInput.current.value
+    const password = this.myPasswordInput.current.value
+
+    if(userName === 'test' && password === 'test') {
+      this.setState({
+        validated: true
+      })
+
+      history.push(`/`)
+    }
+  }
+
+  render() {
+
+    return(
+      <form onSubmit={this.handleSubmit}>
+      <fieldset>
+        <label htmlFor="userName">Username: </label>
+        <input type="text" id="userName" defaultValue="test" ref={this.myUsernameInput} />
+        <label htmlFor="password">Password: </label>
+        <input type="password" id="password" ref={this.myPasswordInput}  />
+        <button type="submit">login</button>
+      </fieldset>
+    </form>
+    )
+  }
+} 
 
 export default Login;
